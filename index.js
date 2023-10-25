15c63e588bdba0645f1666efe2185b9d6ff0309f
Brian <galyenb@vcu.edu>
2023-10-25 12:29:11 -0400

    created questions for overall inquire in index

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
 const inquirer = require("inquirer");
 const shapes = require("/shapes/shapes.js");
+
+const circleQuestions = {};
+const squareQuestions = {};
+const triangleQuestions = {};
+
+const questions = [
+  {
+    type: "input",
+    name: "logoText",
+    message: "Enter up to 3 characters to render on your logo:", ////Going to need a validate here
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message: "Enter the text color for the logo:",
+  },
+  {
+    type: "list",
+    message: "Select a Shape",
+    name: "shape",
+    choices: [Circle, Triangle, Square],
+  },
+  {
+    type: "input",
+    name: "fillColor",
+    message: "Enter the fill color your shape:", ////Validate for color? or use the class?
+  },
+];
+
+function writetoFile() {}
+
+function init() {
+  inquire
+    .prompt(questions)
+    .then((answers) => {
+      writetoFile(answers);
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.error(`Prompt couldn't be rendered in the current environment`);
+      } else {
+        console.error(`Something else went wrong: ${error}`);
+      }
+    });
+}
+
+init();
+
+// <svg
